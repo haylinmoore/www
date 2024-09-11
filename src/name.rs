7deab97af3538e304133742dd5d715e796287818
Haylin Moore <gh@haylinmoore.com>
2024-09-11 14:03:37 -0400

    Remove deadname website version

--- src/name.rs
+++ src/name.rs
@@ -2,31 +2,26 @@ use std::env;
 
 #[derive(Debug, Clone)]
 pub enum Name {
-    Hampton,
     Haylin
 }
 impl Name {
     pub fn from_env() -> Name {
         match env::var("NAME").as_deref() {
-            Ok("Hampton") => Name::Hampton,
             _ => Name::Haylin,
         }
     }
     pub fn uppercase_str(&self) -> &'static str {
         match self {
-            Name::Hampton => "Hampton",
             Name::Haylin => "Haylin"
         }
     }
     pub fn uppercase_full_str(&self) -> &'static str {
         match self {
-            Name::Hampton => "Hampton Moore",
             Name::Haylin => "Haylin Moore"
         }
     }
     pub fn domain(&self) -> &'static str {
         match self {
-            Name::Hampton => "hamptonmoore.com",
             Name::Haylin => "haylinmoore.com"
         }
     }
