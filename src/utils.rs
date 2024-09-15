e7e5243ff48d677bf147a10954343d5adabb5eaf
Haylin Moore <gh@haylinmoore.com>
2024-09-15 16:06:07 -0400

    Replace comrak with markdown-rs

--- src/utils.rs
+++ src/utils.rs
@@ -1,19 +1,17 @@
-use comrak::plugins::syntect::SyntectAdapterBuilder;
-use comrak::{markdown_to_html_with_plugins, ComrakOptions, Plugins};
+use markdown::{to_html_with_options, CompileOptions, Options};
 
 pub fn md_to_html(md: &str) -> String {
-    let mut options = ComrakOptions::default();
-    options.render.unsafe_ = true;
-    options.render.target_blank = true;
-    options.parse.smart = true;
+// Live dangerously / trust the author:
+    let result = to_html_with_options(md, &Options {
+        compile: CompileOptions {
+        allow_dangerous_html: true,
+        allow_dangerous_protocol: true,
 
-    let mut plugins = Plugins::default();
-    let adapter = SyntectAdapterBuilder::new()
-        //.theme("base16-ocean.dark")
-        .css()
-        .build();
-    plugins.render.codefence_syntax_highlighter = Some(&adapter);
-    markdown_to_html_with_plugins(md, &options, &plugins)
-        .trim()
-        .to_string()
+        ..CompileOptions::default()
+        },
+        ..Options::default()
+    });
+
+
+    result.unwrap()
 }
