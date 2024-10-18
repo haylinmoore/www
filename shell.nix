9bd3ada670425c9cb81f37d1420efacd53a034ae
Haylin Moore <me@haylinmoore.com>
2024-10-18 10:49:41 -0400

    badges: add jordan

--- shell.nix
+++ shell.nix
@@ -17,7 +17,7 @@ pkgs.mkShell {
       output_file="''${input_file%.*}.webp"
 
       if [[ "''$input_file" == *.gif ]]; then
-        ffmpeg -i "''$input_file" -pix_fmt bgra -c:v libwebp_anim -lossless 1 -compression_level 6 "''$output_file"
+        ffmpeg -i "''$input_file" -pix_fmt bgra -c:v libwebp_anim -lossless 1 -loop 0 -compression_level 6 "''$output_file"
       else
         ffmpeg -i "''$input_file" -pix_fmt bgra -c:v libwebp -lossless 1 -compression_level 6 "''$output_file"
       fi
